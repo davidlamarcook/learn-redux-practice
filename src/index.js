7e10728035a044476c2bfdb8fda8b9abce4843b6
davidlamarcook <dcook1025@gmail.com>
2022-10-02 20:36:31 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import allReducers from './reducers';
+
+//STORE -> Globalized state
+
+//const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+//ACTION -> give it a name on what you want to do (Incrememnt in this project)
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT' 
+//   }
+// }
+
+// //REDUCER -> CHeck what action you did & then modify store
+
+// const counter = (state = 0, action) => {
+//   switch (action.type){
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//   } 
+// };
+
+// let store = createStore(counter);
+
+// //Display it on the console
+// store.subscribe(() => console.log(store.getState()))
+
+// //DISPATCH -> Dispatch this action to the reducer and update state
+// store.dispatch(increment)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
